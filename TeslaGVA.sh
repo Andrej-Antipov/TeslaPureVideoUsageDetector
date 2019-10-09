f3a56335256d0df0f7a1aa8937fd9e5499ebf4e1
Andrew <36465597+Andrej-Antipov@users.noreply.github.com>
2019-10-09 15:01:46 +0300

    English version bugs fixed

--- TeslaGVA.sh
+++ TeslaGVA.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-#MyPID=`echo $$`
-##string=$(ps -xa | grep TeslaPureVideoUsageDetector | awk '{ print $1 }' | tr '\n' ';')
-#IFS=";"; ResLan=($string); unset IFS; pos=${#ResLan[@]}
-#if [[ ! $pos = 0 ]]; then
-#for (( i=0; i<$pos; i++ )); do 
-#if [[ ! ${ResLan[i]} = $MyPID ]]; then kill ${ResLan[i]} &>/dev/null; fi
-#done
-#fi
-
-#loc=`locale | grep LANG | sed -e 's/.*LANG="\(.*\)_.*/\1/'`
-loc=$(cat ~/.auth/auth.plist | grep -A 1 "Locale" | grep string | sed -e 's/.*>\(.*\)<.*/\1/' | tr -d '\n')
 
 TITLE="PureVideoHD"
 
@@ -28,8 +17,8 @@ client_pid=$(echo "$ionv_ioreg" | cut -f4 -d '"' | cut -f1 -d "," | cut -c 4- | 
             if [[ ${#client} -gt 41 ]]; then client=$( echo "${client:0:41}"); fi
     fi
     if [[ ! $client = "" ]]; then 
-        printf '\r                                                                                \r'
-        if [ $loc = "ru" ]; then
+        loc=`defaults read -g AppleLocale | cut -d "_" -f1`; if [[ ! $loc = "ru" ]]; then loc="en"; fi 
+        if [[ $loc = "ru" ]]; then
 
         SUBTITLE="Клиент ""$client"
         MESSAGE="использует аппаратное ускорение"
@@ -38,26 +27,26 @@ client_pid=$(echo "$ionv_ioreg" | cut -f4 -d '"' | cut -f1 -d "," | cut -c 4- | 
 
         else
 
-        SUBTITLE="The client ""$client"
-        MESSAGE="is using the hardware decoder"
-        COMMAND="display notification \"${MESSAGE}\" with title \"${TITLE}\" subtitle \"${SUBTITLE}\"\""
+        SUBTITLE="$client"
+        MESSAGE="is using decoding hardware acceleration"
+        COMMAND="display notification \"${MESSAGE}\" with title \"${TITLE}\" subtitle \"${SUBTITLE}\""
         osascript -e "${COMMAND}"
+
         fi
 
     else
 
-        if [ $loc = "ru" ]; then
+        if [[ $loc = "ru" ]]; then
         
         osascript -e 'display notification "не используется" with title "PureVideoHD"'
         else
         
-        osascript -e 'display notification "not using" with title "PureVideoHD"'
+        osascript -e 'display notification "is not in use now" with title "PureVideoHD"'
         fi
     fi
 fi
 sleep 1
 done
- sleep 0.5 && osascript -e 'tell application "Terminal" to close first window' & exit
 exit 1
 
 
